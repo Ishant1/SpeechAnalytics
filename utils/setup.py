16c1051205451567de23355ec73e307fc6f03f3b
Ishant Aggarwal <iagg1946@gmail.com>
2023-03-01 13:22:15 +0000

    remove method based setup

--- utils/setup.py
+++ utils/setup.py
@@ -3,22 +3,6 @@ import sys
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 
-
-def colab_setup(topic=''):
-    from google.colab import drive
-
-    drive.mount('/content/gdrive')
-
-    MODEL_DIR = '/content/gdrive/MyDrive/model'
-    DATA_DIR = os.path.join('/content/gdrive/MyDrive/dataset',topic)
-    ERC_DIR = os.path.join('/content/gdrive/MyDrive/Research',topic)
-
-    os.environ['MODEL_DIR'] = MODEL_DIR
-    os.environ['DATA_DIR'] = DATA_DIR
-    os.environ['RESEARCH_DIR'] = ERC_DIR
-    sys.path.append('/content/SpeechAnalytics')
-
-
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument("-t", "--topic", default="ERC")
 
@@ -27,4 +11,16 @@ args = vars(parser.parse_args())
 if __name__ == "__main__":
     topic_name = args['topic']
     print(topic_name)
-    colab_setup(topic=topic_name)
+
+    from google.colab import drive
+
+    drive.mount('/content/gdrive')
+
+    MODEL_DIR = '/content/gdrive/MyDrive/model'
+    DATA_DIR = os.path.join('/content/gdrive/MyDrive/dataset', topic_name)
+    ERC_DIR = os.path.join('/content/gdrive/MyDrive/Research', topic_name)
+
+    os.environ['MODEL_DIR'] = MODEL_DIR
+    os.environ['DATA_DIR'] = DATA_DIR
+    os.environ['RESEARCH_DIR'] = ERC_DIR
+    sys.path.append('/content/SpeechAnalytics')
