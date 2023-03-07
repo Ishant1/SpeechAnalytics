e7d733820532da6a65627baf39439a86b7ed9646
Ishant Aggarwal <iagg1946@gmail.com>
2023-03-07 11:14:42 +0000

    method to standardize results json for different experiments

---
+++ utils/results_utils.py
@@ -0,0 +1,19 @@
+import json
+import time
+
+
+def results_to_json(model_name,dataset_name,results,time_taken,filename):
+    time_of_result = time.strftime("%Y-%m-%d %H:%M:%S")
+
+    json_results = {
+        'ModelName': model_name,
+        'DatasetName': dataset_name,
+        'Results': results,
+        'Timestamp': time_of_result,
+        'RunTime': time_taken,
+    }
+
+    json.dump(json_results, filename)
+    print(f"Results for experiment{model_name}_{dataset_name} has been saved at {filename} at {time_of_result}")
+
+
